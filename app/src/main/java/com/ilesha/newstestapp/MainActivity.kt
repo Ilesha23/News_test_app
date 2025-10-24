11754de45a6c6f650587b98cab63e1762e3acd25
Ilesha23 <ilesha0123@gmail.com>
2025-10-24 23:54:56 +0300

    added application class and hilt annotations

--- app/src/main/java/com/ilesha/newstestapp/MainActivity.kt
+++ app/src/main/java/com/ilesha/newstestapp/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.ilesha.newstestapp.ui.navigation.Navigation
 import com.ilesha.newstestapp.ui.theme.NewsTestAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/ilesha/newstestapp/NewsApp.kt
@@ -0,0 +1,7 @@
+package com.ilesha.newstestapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NewsApp: Application()
