cb02b3aad7f650eba80482f637c1b2b7f793ec84
Ilesha23 <ilesha0123@gmail.com>
2025-10-25 18:50:47 +0300

    added database module to di package

---
+++ app/src/main/java/com/ilesha/newstestapp/di/DatabaseModule.kt
@@ -0,0 +1,41 @@
+package com.ilesha.newstestapp.di
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.ilesha.newstestapp.data.local.dao.ArticleDao
+import com.ilesha.newstestapp.data.local.dao.RemoteKeysDao
+import com.ilesha.newstestapp.data.local.db.NewsDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideNewsDatabase(@ApplicationContext context: Context): RoomDatabase {
+        return Room.databaseBuilder(
+            context,
+            NewsDatabase::class.java,
+            "news.db"
+        ).build()
+    }
+
+    @Provides
+    fun provideArticleDao(newsDatabase: NewsDatabase): ArticleDao {
+        return newsDatabase.articleDao()
+    }
+
+    @Provides
+    fun provideRemoteKeysDao(newsDatabase: NewsDatabase): RemoteKeysDao {
+        return newsDatabase.remoteKeysDao()
+    }
+
+}
