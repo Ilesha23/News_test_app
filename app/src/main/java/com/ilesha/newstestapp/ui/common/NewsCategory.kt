426ed7d57d10dd4fb2bcc700155fb8f47fc20b57
Ilesha23 <ilesha0123@gmail.com>
2025-10-26 09:15:40 +0200

    simplified news categories enum

--- app/src/main/java/com/ilesha/newstestapp/ui/common/NewsCategory.kt
+++ app/src/main/java/com/ilesha/newstestapp/ui/common/NewsCategory.kt
@@ -1,16 +1,15 @@
 package com.ilesha.newstestapp.ui.common
 
-import androidx.annotation.StringRes
 import com.ilesha.newstestapp.R
 
-enum class NewsCategory(val categoryName: String, val stringResId: Int) {
+enum class NewsCategory(val stringResId: Int) {
 
-    BUSINESS("business", R.string.category_business),
-    ENTERTAINMENT("entertainment", R.string.category_entertainment),
-    GENERAL("general", R.string.category_general),
-    HEALTH("health", R.string.category_health),
-    SCIENCE("science", R.string.category_science),
-    SPORTS("sports", R.string.category_sports),
-    TECHNOLOGY("technology", R.string.category_technology),
+    BUSINESS(R.string.category_business),
+    ENTERTAINMENT(R.string.category_entertainment),
+    GENERAL(R.string.category_general),
+    HEALTH(R.string.category_health),
+    SCIENCE(R.string.category_science),
+    SPORTS(R.string.category_sports),
+    TECHNOLOGY(R.string.category_technology),
 
 }
