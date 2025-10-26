cb43ce06e2559707f61082007c0f90651fa06f2e
Ilesha23 <ilesha0123@gmail.com>
2025-10-26 07:33:22 +0200

    added trailing icon to search bar to delete text and remove focus

--- app/src/main/java/com/ilesha/newstestapp/ui/screen/search/SearchScreen.kt
+++ app/src/main/java/com/ilesha/newstestapp/ui/screen/search/SearchScreen.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
@@ -27,8 +29,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberSearchBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -113,6 +119,7 @@ fun SearchPanel(
     modifier: Modifier = Modifier
 ) {
     val focusManager = LocalFocusManager.current
+    var isFocused by remember { mutableStateOf(false) }
     Surface(
         modifier = Modifier
             .padding(vertical = 5.dp)
@@ -140,7 +147,25 @@ fun SearchPanel(
                             contentDescription = stringResource(R.string.search_screen_search_bar_icon)
                         )
                     },
+                    trailingIcon = {
+                        if (isFocused || searchText.isNotEmpty()) {
+                            IconButton(
+                                onClick = {
+                                    onSearchTextChanged("")
+                                    focusManager.clearFocus()
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Clear,
+                                    contentDescription = Icons.Outlined.Clear.name
+                                )
+                            }
+                        }
+                    },
                     modifier = modifier
+                        .onFocusChanged { focusState ->
+                            isFocused = focusState.isFocused
+                        }
                 )
             }
         )
