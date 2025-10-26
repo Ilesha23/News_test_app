bc8006b2c097b35656fe652212d4a04921bb0b30
Ilesha23 <ilesha0123@gmail.com>
2025-10-26 07:14:10 +0200

    added search screen and its viewmodel, added strings to resources

--- app/src/main/java/com/ilesha/newstestapp/ui/screen/search/SearchViewModel.kt
+++ app/src/main/java/com/ilesha/newstestapp/ui/screen/search/SearchViewModel.kt
@@ -1,15 +1,46 @@
 package com.ilesha.newstestapp.ui.screen.search
 
 import androidx.lifecycle.ViewModel
-import com.ilesha.newstestapp.domain.repository.NewRepository
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.ilesha.newstestapp.domain.usecase.GetPagedNewsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    private val newsRepository: NewRepository
+    private val getPagedNewsUseCase: GetPagedNewsUseCase
 ) : ViewModel() {
 
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
 
+    private val query = MutableStateFlow("")
+
+    val articles = query
+        .flatMapLatest {
+            if (it.isEmpty()) {
+                flowOf(PagingData.empty())
+            } else {
+                getPagedNewsUseCase(it)
+            }
+        }
+        .cachedIn(viewModelScope)
+
+    fun onSearchTextChange(text: String) {
+        _searchText.update { text }
+    }
+
+    fun search() {
+        query.update { _searchText.value.trim() }
+    }
 
 }
