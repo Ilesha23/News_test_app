e605648c7ca595a4d517b2b494d23dd486259d1f
Ilesha23 <ilesha0123@gmail.com>
2025-10-26 09:23:32 +0200

    added categories screen

--- app/src/main/java/com/ilesha/newstestapp/ui/screen/categories/CategoriesScreen.kt
+++ app/src/main/java/com/ilesha/newstestapp/ui/screen/categories/CategoriesScreen.kt
@@ -1,10 +1,88 @@
 package com.ilesha.newstestapp.ui.screen.categories
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.ilesha.newstestapp.domain.model.Article
+import com.ilesha.newstestapp.ui.common.NewsCategory
+import com.ilesha.newstestapp.ui.common.NewsContent
 
 @Composable
-fun CategoriesScreen(
+fun CategoriesScreen() {
 
+    val viewModel: CategoriesViewModel = hiltViewModel()
+    val selectedCategory by viewModel.selectedCategory.collectAsStateWithLifecycle()
+    val articles = viewModel.articles.collectAsLazyPagingItems()
+
+    CategoryScreenContent(
+        articles = articles,
+        selectedCategory = selectedCategory,
+        allCategories = NewsCategory.entries,
+        onCategorySelected = viewModel::onCategoryChanged
+    )
+}
+
+@Composable
+fun CategoryScreenContent(
+    articles: LazyPagingItems<Article>,
+    selectedCategory: NewsCategory,
+    allCategories: List<NewsCategory>,
+    onCategorySelected: (NewsCategory) -> Unit
 ) {
 
+    Column {
+        CategorySelectionRow(
+            selectedCategory = selectedCategory,
+            allCategories = allCategories,
+            onCategorySelected = onCategorySelected
+        )
+        NewsContent(
+            articles = articles,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
+        )
+    }
+
+}
+
+@Composable
+fun CategorySelectionRow(
+    selectedCategory: NewsCategory,
+    allCategories: List<NewsCategory>,
+    onCategorySelected: (NewsCategory) -> Unit
+) {
+    FlowRow(
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        allCategories.forEach { category ->
+            FilterChip(
+                selected = selectedCategory == category,
+                onClick = {
+                    onCategorySelected(category)
+                },
+                label = {
+                    Text(
+                        text = stringResource(category.stringResId)
+                    )
+                }
+            )
+        }
+    }
 }
--- app/src/main/java/com/ilesha/newstestapp/ui/screen/categories/CategoriesViewModel.kt
+++ app/src/main/java/com/ilesha/newstestapp/ui/screen/categories/CategoriesViewModel.kt
@@ -1,11 +1,35 @@
 package com.ilesha.newstestapp.ui.screen.categories
 
 import androidx.lifecycle.ViewModel
-import com.ilesha.newstestapp.domain.repository.NewRepository
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.ilesha.newstestapp.domain.usecase.GetPagedNewsByCategoryUseCase
+import com.ilesha.newstestapp.ui.common.NewsCategory
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
+@HiltViewModel
 class CategoriesViewModel @Inject constructor(
-    private val newsRepository: NewRepository
+    private val getPagedNewsByCategoryUseCase: GetPagedNewsByCategoryUseCase
 ) : ViewModel() {
+
+    private val _selectedCategory = MutableStateFlow(NewsCategory.GENERAL)
+    val selectedCategory = _selectedCategory.asStateFlow()
+
+    val articles = _selectedCategory
+        .flatMapLatest {
+            getPagedNewsByCategoryUseCase(it.name)
+        }
+        .cachedIn(viewModelScope)
+
+    fun onCategoryChanged(category: NewsCategory) {
+        _selectedCategory.update { category }
+    }
 
 }
