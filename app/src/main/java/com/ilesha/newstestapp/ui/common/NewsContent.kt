a2ae8dda223d8feea81918fdd1c4e2111661050a
ilesha23 <ilesha0123@gmail.com>
2025-10-27 04:01:02 +0200

    added ability to open article in browser

--- app/src/main/java/com/ilesha/newstestapp/ui/common/NewsContent.kt
+++ app/src/main/java/com/ilesha/newstestapp/ui/common/NewsContent.kt
@@ -1,5 +1,6 @@
 package com.ilesha.newstestapp.ui.common
 
+import android.content.Intent
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -16,6 +17,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
@@ -37,14 +39,22 @@ import com.ilesha.newstestapp.domain.model.Article
 import com.ilesha.newstestapp.domain.model.Source
 import com.ilesha.newstestapp.utils.ext.formatToLocaleString
 import java.time.Instant
+import androidx.core.net.toUri
 
 @Composable
 fun NewsContent(
     articles: LazyPagingItems<Article>,
     modifier: Modifier = Modifier
 ) {
-    val loadStateLocal = articles.loadState.mediator?.refresh
+    val context = LocalContext.current
+    val openBrowser: (String) -> Unit = remember(context) {
+        { url ->
+            val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+            context.startActivity(intent)
+        }
+    }
 
+    val loadStateLocal = articles.loadState.mediator?.refresh
     when {
         loadStateLocal is LoadState.Error -> {
             NewsContentMessageBox(
@@ -80,6 +90,7 @@ fun NewsContent(
             NewsList(
                 articles = articles,
                 loadState = articles.loadState.append,
+                onArticleClick = openBrowser,
                 modifier = Modifier
                     .fillMaxSize()
             )
@@ -107,7 +118,8 @@ fun NewsContentMessageBox(
 fun NewsList(
     articles: LazyPagingItems<Article>,
     loadState: LoadState,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onArticleClick: (String) -> Unit
 ) {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(2.dp),
@@ -119,7 +131,8 @@ fun NewsList(
         ) { index ->
             articles[index]?.let { article ->
                 ArticleCardItem(
-                    article = article
+                    article = article,
+                    onArticleClick = onArticleClick
                 )
             }
         }
@@ -139,14 +152,15 @@ fun NewsList(
 
 @Composable
 fun ArticleCardItem(
-    article: Article
+    article: Article,
+    onArticleClick: (url: String) -> Unit
 ) {
     Card(
         shape = RectangleShape,
         modifier = Modifier
             .clickable(
                 onClick = {
-
+                    onArticleClick(article.url)
                 }
             )
     ) {
@@ -220,6 +234,7 @@ fun ArticleCardItemPreview() {
             urlToImage = "https://longlonglink.com",
             publishedAt = Instant.now(),
             content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget dolor ultricies, varius magna eget, venenatis arcu. Vivamus libero sem, blandit vel tincidunt vitae, bibendum ac diam. Fusce vitae orci a nisl iaculis ultrices. Etiam ut congue tellus. Donec faucibus tortor vitae ligula pretium, eu molestie orci tempus. Cras porta arcu nisi, nec pretium est hendrerit id. Pellentesque posuere arcu non lorem efficitur, et pellentesque risus euismod. Quisque blandit fringilla sapien a efficitur. Curabitur viverra nisl sit amet aliquam pretium. Morbi egestas, libero ac tincidunt bibendum, velit ligula vulputate nunc, a suscipit sapien odio at purus. Nunc sed pretium ligula, eget accumsan nibh. Duis egestas laoreet dolor, non ultrices turpis feugiat vitae. Aenean odio dui, luctus vitae facilisis eget, ullamcorper ut magna. Etiam vehicula sit amet eros vitae volutpat. In feugiat, diam et molestie laoreet, tellus purus posuere neque, vel ullamcorper orci enim sagittis ipsum. Sed ac volutpat dui, vel dignissim erat.",
-        )
+        ),
+        onArticleClick = {}
     )
 }
